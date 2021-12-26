042ae0fbff4704e64be3c6d3dd8533f66eaa983d
TudbuT <tudbut@tudbut.de>
2021-12-26 20:34:40 +0100

    Fixed stream.sh and added more write functions

--- stream.sh
+++ stream.sh
@@ -6,32 +6,39 @@
 # Made by TudbuT and licensed under GPL version 3
 
 PID=$$
-touch "$PID.out" "$PID.in" "$PID.in.buf"
-tail -f "$PID.out" | $pargs > "$PID.in.buf" &
+touch "$PID.out" "$PID.in"
+tail -f "$PID.out" | $(echo $pargs) | (
+    while true ; do
+        read -r line || exit
+        [ "$line" = '' ] && continue 
+        echo "$line" >> "$PID.in"
+        unset line
+    done 
+) > /dev/null 2>&1 &
 ncpid=$!
-tail -fn+1 "$PID.in.buf" | while true ; do
-    read -r line
-    echo "$line" >> "$PID.in"
-done > /dev/null 2>&1 &
 readerpid=$!
 (
-    while kill -0 "$PID" ; do 
+    while kill -0 "$PID" "$ncpid" ; do 
         sleep 0.1
     done
     # Main process got killed
-    sleep 0.1
-    kill "$ncpid" "$readerpid"
-    sleep 0.1
-    rm -rf "$PID.in" "$PID.in.buf" "$PID.out"
+    kill "$PID" "$ncpid" "$readerpid"
+    rm -rf "$PID.in" "$PID.out"
 ) > /dev/null 2>&1 & 
+write() {
+    echo -n "$@" >> "$PID.out"
+}
+writee() {
+    echo -ne "$@" >> "$PID.out"
+}
 writeln() {
     echo "$@" >> "$PID.out"
 }
-write() {
-    echo -n "$@" >> "$PID.out"
+writelne() {
+    echo -e "$@" >> "$PID.out"
 }
 readln() {
     tail -fn+1 "$PID.in" | head -n1 2> /dev/null
     tail -n+2 "$PID.in" > "$PID.in.tmp" 2> /dev/null
-    mv "$PID.in.tmp" "$PID.in"
+    mv "$PID.in.tmp" "$PID.in" 2> /dev/null
 }
